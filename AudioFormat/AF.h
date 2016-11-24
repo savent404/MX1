dee526e12036029f932210e82f9f03fdc880b9c2
savent <savent_gate@outlook.com>
2016-11-24 13:49:46 +0800

    NEW(AudioFormat):add std file

---
+++ AudioFormat/AF.h
@@ -0,0 +1,16 @@
+#ifndef _AF_H_
+#define _AF_H_
+
+/* This port will deal with ".wav" formate */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ AudioFormat/test/main.c
@@ -0,0 +1,6 @@
+#include "AF.H"
+
+int main(int argc, char argc[]) {
+
+    return 0;
+}
