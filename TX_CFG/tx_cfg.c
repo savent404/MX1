959475e50b5665c0ce8686baa2dd09e7b53669c9
廖元垲 <Savent_gate@outlook.com>
2016-12-21 22:55:53 +0800

    Feature-Config file read
    Test in System-Linux.

--- TX_CFG/tx_cfg.c
+++ TX_CFG/tx_cfg.c
@@ -1,18 +1,59 @@
 #include "tx_cfg.h"
 
 const char name_string[][10] = {
-    "vol",
+    "Vol",
+    "Bank",
+    "Tpon",
+    "Tpoff",
+    "Tout",
+    "Tin",
+    "Ts_switch",
+    "Tautoin",
+    "Tautooff",
+    "Tmute",
+    "TLcolor",
+    "TBfreeze",
+    "TCfreeze",
+    "TCflip",
+    "Ccount",
+    "TDfreeze",
+    "TDflip",
+    "Dcount",
+    "TEtrigger",
+    "Ecycle",
+    "TLon",
+    "TLOff",
+    "Lbright",
+    "Lmode",
+    "memsHz",
+    "memsA",
+    "memsThr",
+    "memsGap",
+    "Sl",
+    "Sh",
+    "Cl",
+    "Ch",
 };
 
 void TX_CFG(struct config *cfg) {
-    FIL fp;
+    
     char Lbuf[20];
     char name[10], val[10];
     int  pos = 0;
+    #if LINUX_TEST
+    int fd;
+    fd = open(DEFAULT_TEXT_PATH, O_RDONLY);
+    if (fd <= 0) {
+        return;
+    }
+    while (TextLine(fd, Lbuf) != 0) {
+    #else
+    FIL fp;
     if (f_open(&fp, DEFAULT_TEXT_PATH, FA_READ) != FR_OK) {
         return;
     }
     while (TextLine(&fp, Lbuf) != 0) {
+    #endif
         TextNameVal(Lbuf, name, val);
         for (pos = 0; pos < VAR_NUM; pos++) {
             if (strcasecmp(name, name_string[pos]))
@@ -22,15 +63,55 @@ void TX_CFG(struct config *cfg) {
         // match
         switch(pos) {
             // val
-            case 0: cfg->vol = atoi(val); break;
-            /*
-            case VAR_NUM - 1: berak;
-            */
-            default:
+            case 0: cfg->Vol = atoi(val); break;
+            case 1: cfg->Bank = atoi(val); break;
+            case 2: cfg->Tpon = atoi(val); break;
+            case 3: cfg->Tpoff = atoi(val); break;
+            case 4: cfg->Tout = atoi(val); break;
+            case 5: cfg->Tin = atoi(val); break;
+            case 6: cfg->Ts_switch = atoi(val); break;
+            case 7: cfg->Tautoin = atoi(val); break;
+            case 8: cfg->Tautooff = atoi(val); break;
+            case 9: cfg->Tmute = atoi(val); break;
+            case 10: cfg->TLcolor = atoi(val); break;
+            case 11: cfg->TBfreeze = atoi(val); break;
+            case 12: cfg->TCfreeze = atoi(val); break;
+            case 13: cfg->TCflip = atoi(val); break;
+            case 14: cfg->Ccount = atoi(val); break;
+            case 15: cfg->TDfreeze = atoi(val); break;
+            case 16: cfg->TDflip = atoi(val); break;
+            case 17: cfg->Dcount = atoi(val); break;
+            case 18: cfg->TEtrigger = atoi(val); break;
+            case 19: cfg->Ecycle = atoi(val); break;
+            case 20: cfg->TLon = atoi(val); break;
+            case 21: cfg->TLoff = atoi(val); break;
+            case 22: cfg->Lbright = atoi(val); break;
+            case 23: cfg->Lmode = atoi(val); break;
+            case 24: cfg->memsHz = atoi(val); break;
+            case 25: cfg->memsA = atoi(val); break;
+            case 26: cfg->memsThr = atoi(val); break;
+            case 27: cfg->memsGap = atoi(val); break;
+            case 28: cfg->S1 = atoi(val); break;
+            case 29: cfg->Sh = atoi(val); break;
+            case 30: cfg->Cl = atoi(val); break;
+            case 31: cfg->Ch = atoi(val); break;
         }
     }
 }
-
+#if LINUX_TEST
+static int TextLine(int fd, char *des) {
+    char chara[1];
+    int num = 1;
+    read(fd, chara, 1);
+    while (*chara != '\n') {
+        *des++ = *chara;
+        read(fd, chara, 1);
+        num += 1;
+    }
+    *des = '\0';
+    return num;
+}
+#else
 static int TextLine(FIL *pt, char *des) {
     char chara[1];
     UINT cnt = 0;
@@ -46,6 +127,7 @@ static int TextLine(FIL *pt, char *des) {
     *des = '\0';
     return num;
 }
+#endif
 
 static void TextNameVal(char *line, char *name, char *val) {
     while (*line != '=') {
